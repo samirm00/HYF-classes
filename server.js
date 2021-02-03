aaec45c48b77ddd2337067817ce52d119b3cffa3
samirm00 <sammoham.be@gmail.com>
2021-02-03 01:26:42 +0100

    try to deploy on hereoku

--- server.js
+++
@@ -1,5 +0,0 @@
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
--- src/init/index.js
+++ src/init/index.js
@@ -1,4 +1,5 @@
 'use strict';
+var port = process.env.PORT || 5000 ;
 
 import '../listeners/listEnter.js'
 
